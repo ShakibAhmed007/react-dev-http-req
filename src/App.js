0348eff556ca722f3d797d8c2c7624bafbeecfdd
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-01 11:02:43 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React, { useState } from 'react';
 import './style.css';
 
 export default function App() {
+  // movies state
   const [movies, setMovies] = useState([]);
+
+  // loading state
+  const [isLoading, setIsLoading] = useState(false);
 
   // req
   async function fetchMovieHandler() {
+    setIsLoading(true);
     const response = await fetch('https://swapi.dev/api/films/');
     const data = await response.json();
     const transformedData = data.results.map(movie => {
@@ -15,20 +20,26 @@ export default function App() {
       };
     });
     setMovies(transformedData);
+    setIsLoading(false);
+  }
+
+  let data = <p>Loading .... </p>;
+  if (!isLoading) {
+    data = movies.map(movie => {
+      return (
+        <div>
+          <div>{movie.id}</div>
+          <div>{movie.title}</div>
+        </div>
+      );
+    });
   }
 
   return (
     <div>
       <h1>React Http Requests</h1>
       <button onClick={fetchMovieHandler}> Fetch Movies </button>
-      {movies.map(movie => {
-        return (
-          <div>
-            <div>{movie.id}</div>
-            <div>{movie.title}</div>
-          </div>
-        );
-      })}
+      {data}
     </div>
   );
 }
