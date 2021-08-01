8175bdc50e2a68f5345e45e7b453a84b5f2fc89b
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-01 10:45:08 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
+  const [movies, setMovies] = useState([]);
+  function fetchMovieHandler() {
+    fetch('https://swapi.dev/api/films/')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        const transformedData = data.results.map(movie => {
+          return {
+            id: movie.episode_id,
+            title: movie.title
+          };
+        });
+        setMovies(transformedData);
+      })
+      .catch();
+  }
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>React Http Requests</h1>
+      <button onClick={fetchMovieHandler}> Fetch Movies </button>
+      {movies.map(movie => {
+        return (
+          <div>
+            <div>{movie.id}</div>
+            <div>{movie.title}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
