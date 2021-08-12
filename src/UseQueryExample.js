e7dab63c75edbefd2e71007fe55ca8597b8233de
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-12 16:08:10 +0600

    1 file modified

--- src/UseQueryExample.js
+++ src/UseQueryExample.js
@@ -5,7 +5,15 @@ const UseQueryExample = () => {
   const { refetch } = useQuery('repoData', () => {
     fetch('https://dev-http-default-rtdb.firebaseio.com/test_data.json')
       .then(res => {
-        console.log('use Query Response', res);
+        const data = res.json();
+        const dataArr = [];
+        for (const key in data) {
+          dataArr.push({
+            id: data[key].id,
+            title: data[key].title
+          });
+        }
+        console.log('use Query Response', dataArr);
       })
       .catch(error => {
         console.log(error);
