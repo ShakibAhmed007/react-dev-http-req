e15f6b5439ee66c34e19309f9b1be7a591048dc5
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-12 16:01:02 +0600

    2 files modified

--- src/UseQueryExample.js
+++ src/UseQueryExample.js
@@ -1,21 +1,26 @@
 import React, { useEffect } from 'react';
+import { useQuery } from 'react-query';
 
 const UseQueryExample = () => {
-  const getData = () => {
-    try {
-      const data = fetch(
-        'https://dev-http-default-rtdb.firebaseio.com/test_data.json'
-      ).then(res => {
-        console.log(res);
-      });
-    } catch (error) {
-      console.log('error ---', error);
-    }
-  };
+  const { refetch, isLoading, error, data } = useQuery('', () => {});
+
+  // const getData = () => {
+
+  //     const { isLoading, error, data } = useQuery(
+  //       'repoData',
+  //       fetch(
+  //         'https://dev-http-default-rtdb.firebaseio.com/test_data.json'
+  //       ).then(res => {
+  //         console.log('use Query Response', res);
+  //       })
+  //     );
+
+  // };
 
   useEffect(() => {
     console.log('Use Effect Hooks !!!');
-    getData();
+    // getData();
+    refetch();
   }, []);
 
   return <div>Use Query Component Works !!!</div>;
