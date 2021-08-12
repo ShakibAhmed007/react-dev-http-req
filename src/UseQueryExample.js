2f926b19872c56829bc162b2c2026cdf1502b060
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-12 16:02:48 +0600

    2 files modified

--- src/UseQueryExample.js
+++ src/UseQueryExample.js
@@ -2,24 +2,16 @@ import React, { useEffect } from 'react';
 import { useQuery } from 'react-query';
 
 const UseQueryExample = () => {
-  const { refetch, isLoading, error, data } = useQuery('', () => {});
-
-  // const getData = () => {
-
-  //     const { isLoading, error, data } = useQuery(
-  //       'repoData',
-  //       fetch(
-  //         'https://dev-http-default-rtdb.firebaseio.com/test_data.json'
-  //       ).then(res => {
-  //         console.log('use Query Response', res);
-  //       })
-  //     );
-
-  // };
+  const { refetch, isLoading, error, data } = useQuery('repoData', () => {
+    fetch('https://dev-http-default-rtdb.firebaseio.com/test_data.json').then(
+      res => {
+        console.log('use Query Response', res);
+      }
+    );
+  });
 
   useEffect(() => {
     console.log('Use Effect Hooks !!!');
-    // getData();
     refetch();
   }, []);
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
- // Create a client
- const queryClient = new QueryClient();
-
+// Create a client
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <QueryClientProvider client={queryClient}>
