d52082ce25559abad002dd1a475d9ffa67add1b5
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-12 16:59:34 +0600

    1 file modified

--- src/UseQueryExample.js
+++ src/UseQueryExample.js
@@ -1,25 +1,30 @@
 import React, { useEffect } from 'react';
 import { useQuery } from 'react-query';
 
-const UseQueryExample = () => {
-  const fetchMovies = () => {
-    fetch('https://api.github.com/repos/tannerlinsley/react-query').then(
-      res => {
-        const data = res.json();
-        const dataArr = [];
-        for (const key in data) {
-          dataArr.push({
-            id: data[key].id,
-            title: data[key].title
-          });
-        }
-        console.log('data ---', dataArr);
-        return res.json();
-      }
+const UseQueryExample = async () => {
+  const fetchMovieHandler = async () => {
+    const response = await fetch(
+      'https://dev-http-default-rtdb.firebaseio.com/test_data.json'
     );
+    if (!response.ok) {
+      throw new Error('Something went wrong');
+    }
+    const data = await response.json();
+    const dataArr = [];
+    for (const key in data) {
+      dataArr.push({
+        id: data[key].id,
+        title: data[key].title
+      });
+    }
+    console.log(dataArr);
   };
 
-  const { isLoading, isError, data, error } = useQuery('repoData', fetchMovies);
+  // const { isLoading, isError, data, error } = useQuery('repoData', fetchMovies);
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   // console.log(isLoading, isError, data, error);
   return <div>Use Query Component Works !!!</div>;
